0a386c8ee5162594b3a1d4222838ce0a0020b974
ruegerj <jan.rueger@gmx.ch>
2022-12-02 11:47:49 +0100

    refactor loading of daily inputs

--- day-01/day01.go
+++ day-01/day01.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path"
+
+	"github.com/ruegerj/aoc-2022/util"
 )
 
 func main() {
-	cwd, err := os.Getwd()
-
-	if err != nil {
-		fmt.Println("Error while fetching current dir", err)
-		return
-	}
-
-	rawInput, err := os.ReadFile(path.Join(cwd, "day-01", "01.txt"))
-	input := string(rawInput)
-
-	if err != nil {
-		fmt.Println("Error while reading file", err)
-		return
-	}
+	input := util.LoadDailyInput(1)
 
 	fmt.Printf("Part 1: %d\n", Part1(input))
 	fmt.Printf("Part 2: %d\n", Part2(input))
--- day-02/day02.go
+++ day-02/day02.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path"
+
+	"github.com/ruegerj/aoc-2022/util"
 )
 
 const Rock = "rock"
@@ -17,20 +17,7 @@ var operandScores = map[string]int{
 }
 
 func main() {
-	cwd, err := os.Getwd()
-
-	if err != nil {
-		fmt.Println("Error while fetching current dir", err)
-		return
-	}
-
-	rawInput, err := os.ReadFile(path.Join(cwd, "day-02", "02.txt"))
-	input := string(rawInput)
-
-	if err != nil {
-		fmt.Println("Error while reading file", err)
-		return
-	}
+	input := util.LoadDailyInput(2)
 
 	fmt.Printf("Part 1: %d\n", Part1(input))
 	fmt.Printf("Part 2: %d\n", Part2(input))
